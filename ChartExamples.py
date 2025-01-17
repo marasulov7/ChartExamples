bcbeba63b019bf3f019bbf679da113faa8840bd0
marasulov7 <marasulovalish@gmail.com>
2025-01-17 11:28:14 +0500

    Добавление кнопки и метки для графика 2

--- ChartExamples.py
+++ ChartExamples.py
@@ -23,6 +23,14 @@ btn_chart1.place(x=40, y=115, width=90, height=30)
 lbl_chart1 = tk.Label(text="График синуса matplotlib")
 lbl_chart1.place(x=170, y=122)
 
+
+# Добавление кнопки и метки для графика 2
+btn_chart2 = tk.Button(window, text="График 2", font = ('Helvetica', 10, 'bold'))
+btn_chart2.place(x=40, y=165, width=90, height=30)
+
+lbl_chart2 = tk.Label(text="Нормальное распределение")
+lbl_chart2.place(x=170, y=172)
+
 # Добавление кнопки закрытия программы
 btnClose = tk.Button(window, text = "Закрыть", font = ('Helvetica', 10, 'bold'), command=do_close) # шрифт, размер, жирный+вызов ф закр окна
 btnClose.place(x=330, y=400, width=90, height=30) #функция place размещает элемент в нужной точке+ширина и высота эл-та
